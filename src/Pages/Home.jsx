e406eb961dabda1225828842031b9a2956ad5803
G-code <30412971+genciiv@users.noreply.github.com>
2024-01-03 21:02:56 +0100

    create newsletters (right sidebar)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Jobs from "./Jobs";
 import Sidebar from "../sidebar/Sidebar";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -140,7 +141,9 @@ const Home = () => {
         </div>
 
         {/* right side */}
-        <div className="bg-white p-4 rounded">Right</div>
+        <div className="bg-white p-4 rounded">
+          <Newsletter/>
+        </div>
       </div>
     </div>
   );
